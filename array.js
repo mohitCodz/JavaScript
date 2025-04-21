8ccabff36d6d66c11bc87c03cfb2cbe88c6f5c2d
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-04-21 18:15:44 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+//  let superHero = ["Superman,Ironman,Spiderman"];
+
+// for (let el of superHero){
+//     console.log(el);
+// }
+//  
+
+
+let car =["BMW", "Mercedes", "Audi"];
+
+for (let ca of car ){
+     console.log(ca.toUpperCase());
+}
