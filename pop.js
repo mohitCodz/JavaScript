6c05785b109a7dbaf700d785b35d47c234a6ba38
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-06-08 15:02:54 +0530

    Update pop.js

---
+++ pop.js
@@ -0,0 +1,4 @@
+let carNames = ["Toyota", "Honda", "Ford", "Chevrolet", "Nissan"];
+console.log("Original car names: " + carNames.join(", "));
+carNames.pop();
+console.log("Updated car names: " + carNames.join(", "));
