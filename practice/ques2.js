428b6074f3ae84aac16b973c04c8f964b2c6d8cb
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-07-16 03:49:17 +0530

    Update ques2.js

--- practice/ques2.js
+++ practice/ques2.js
@@ -1,1 +1,5 @@
 // create a function using " function " keyword that takes a string as an argument and return the no. of vowels in the string using arrow function
+
+function countVowels(str) => {
+    
+}
