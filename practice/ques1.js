db6ead9e4fed39c439053e99bd41c46729266d41
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-07-16 03:32:43 +0530

    Update ques1.js

--- practice/ques1.js
+++ practice/ques1.js
@@ -1,1 +1,5 @@
-// 
+// create a function using " function " keyword that takes a string as an argument and return the no. of vowels in the string
+
+function countVowels(string){
+    for( let )
+}
