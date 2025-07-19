99d7074ba0906908f118cc5b965fbe6203959c86
Mohit ThakurS16 <mohitjaryalthakur@gmail.com>
2025-07-20 03:09:46 +0530

    Update forEach.js

--- practice/forEach.js
+++ practice/forEach.js
@@ -1,5 +1,5 @@
 // forEach loop example
  let arr = ["ram" ,"shayam","raam"];
  arr.forEach(function printValue(val)){ // this call back function will execute for each item 
- console.log
+ console.log(val);
  }
