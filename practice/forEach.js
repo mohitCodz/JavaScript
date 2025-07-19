93aae8be4c636627f83abba24249fd70e343bc1b
Mohit <mohitjaryalthakur@gmail.com>
2025-07-20 03:33:37 +0530

    Delete practice/forEach.js

--- practice/forEach.js
+++
@@ -1,5 +0,0 @@
-// forEach loop example
- let arr = ["ram" ,"shayam","raam"];
- arr.forEach(function printValue(val){ // this call back function will execute for each item 
- console.log(val);
- }); 
