c556d5888ee088e36fc6ca065e8524f03131c0b3
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-20 03:31:23 +0530

    Update forEachArrow.js

--- practice/forEachArrow.js
+++ practice/forEachArrow.js
@@ -1,5 +1,5 @@
 // for each arrow function
 let arr =["hello","how","are","you"];
 arr.forEach((val)=> { // now i have used arrow function and now this will execute for each item
-    console.log(val)
+    console.log(val);
 });
