cfb3f5f29302c5a669a5981f79e3c0a2052fae35
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-20 04:28:29 +0530

    Update map.js

--- functions&Methods/map.js
+++ functions&Methods/map.js
@@ -1,5 +1,5 @@
 // map function example ( find the square of the array )
 let nums =[1,2,3,4];
-arr.map((val,indx)=>{
-    console.log(val,indx); // this will print the square of the each value of an array and the index value
+arr.map((val)=>{
+    console.log(val); // this will print the square of the each value of an array and the index value
 });
