1dd2405cc09abb3360812cd0b746cbe99cb76b99
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-20 04:09:20 +0530

    Create forEachArrow.js

---
+++ functions&Methods/forEachArrow.js
@@ -0,0 +1,5 @@
+// for each arrow function
+let arr =["hello","how","are","you"];
+arr.forEach((val,indx)=> { // now i have used arrow function and now this will execute for each item
+    console.log(val.toUpperCase(), indx);
+});
