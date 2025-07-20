7c18f2c40bd48966efa6a3ccee98730a3a826f83
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-21 01:37:58 +0530

    Update ques4.js

--- practice/ques4.js
+++ practice/ques4.js
@@ -1,3 +1,7 @@
 // in an given array . filter out the marks of students hat scored 90+
 
 let arr =[23,90,98,9,6,94,22,99,100];
+const output = marks.filter((val)=>{
+    return val > 90; // returning the value that is greater than 90
+});
+console.log(output);
