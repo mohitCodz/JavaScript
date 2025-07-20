ab0a7d6921bac05675ec22a05ab6b175bc43d885
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-21 01:25:34 +0530

    Update reduce.js

--- functions&Methods/reduce.js
+++ functions&Methods/reduce.js
@@ -1,5 +1,5 @@
 // program to show the use of the reduce method
 let arr=[1,2,3,4];
-arr.reduce((res,curr){
-    
-})
+arr.reduce((res,curr)=>{
+ return res + curr; // adding the result and current 
+});
