cbeaffca69095d3ee3ca26044f62a94371fa8628
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-21 01:32:40 +0530

    Update reduce.js

--- functions&Methods/reduce.js
+++ functions&Methods/reduce.js
@@ -6,8 +6,8 @@ const output = arr.reduce((res,curr)=>{
 console.log(output);
 
 // to find out the largest number in an array 
-let arr=[1,2,3,4];
-const output = arr.reduce((res,curr)=>{
- return res > curr
-});
-console.log(output);
+// let arr=[1,2,3,4];
+// const output = arr.reduce((res,curr)=>{
+//  return res > curr ? res : curr; 
+// });
+// console.log(output);
