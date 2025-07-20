8ab24d4c24f6ee49b379bb51a33b9e1c3d40cde6
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-21 01:51:32 +0530

    Update ques5.js

--- practice/ques5.js
+++ practice/ques5.js
@@ -5,3 +5,9 @@ for(let i = 1; i>=n;i++){
  arr[i-1]=i; // i-1 store the first value in the first index and so on 
 }
 console.log(arr); // printing the array
+
+// finding the sum of all numbers using reduce meathod
+
+const output=arr.reduce((result,current)=>{
+    return result + current;
+})
