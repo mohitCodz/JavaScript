23fd6198d2adf9545632d41b9555bff2e2f13801
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-21 01:49:27 +0530

    Update ques5.js

--- practice/ques5.js
+++ practice/ques5.js
@@ -1,5 +1,5 @@
 // take array input form user and print sum
-let n = prompt("Enter an array"); // asking 
+let n = prompt("Enter an array"); // asking user for input 
 let arr=[];
 for(let i = 1; i>=n;i++){
  arr[i-1]=i; // i-1 store the first value in the first index and so on 
