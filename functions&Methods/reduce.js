e03d580446a53d76d221da7f74c7654363830e84
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-21 01:32:12 +0530

    Update reduce.js

--- functions&Methods/reduce.js
+++ functions&Methods/reduce.js
@@ -8,6 +8,6 @@ console.log(output);
 // to find out the largest number in an array 
 let arr=[1,2,3,4];
 const output = arr.reduce((res,curr)=>{
- return res + curr; // adding the result and current 
+ return res > curr
 });
 console.log(output);
