1d12d1c8ae64b8a54ad34ce6aea13c5fc3c10f15
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-22 03:06:04 +0530

    Update script.js

--- functions&Methods/DOM/script.js
+++ functions&Methods/DOM/script.js
@@ -5,4 +5,6 @@ h2.innerText = h2.innerText + "from Mohit";
 
 
 let div = document.querySelectorAll(".box");
-console.log(div[0]);
+div[0].innerText="Hello World"; // div[0]-> we are accessing the first element of the 
+div[1].innerText="I am learning JavaScript!";
+div[2].innerText="Keep Coding";
