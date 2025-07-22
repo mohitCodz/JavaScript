cdcc5e5ccc26ee1529d84f4c7a28fec58439a1e0
mohitCodz <mohitjaryalthakur@gmail.com>
2025-07-23 04:39:29 +0530

    Create script.js

---
+++ DOM/script.js
@@ -0,0 +1,19 @@
+// Ques - create three divs with common name "box". Access them & add some unique text 
+
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + "from Mohit";
+
+// this is the more efiitient way to solve this ques
+let divs = document.querySelectorAll(".box"); // Use 'divs' for the collection
+let idx = 1;
+for ( div of divs){ // 'div' is the current item
+  div.innerText = `new unique value ${idx}`;
+  idx++;
+}
+
+
+// div[0].innerText="Hello World";  // div[0]  we are accessing the first element 
+// div[1].innerText="I am learning JavaScript!";  // div[1]  we are accessing the second element 
+// div[2].innerText="Keep Coding";  // div[2]  we are accessing the third element 
