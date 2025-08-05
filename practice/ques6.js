87ae5c083c2eede4ecd0e803d2364e61a54771eb
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-06 03:39:24 +0530

    Update ques6.js

--- practice/ques6.js
+++ practice/ques6.js
@@ -1,5 +1,7 @@
 // create a toggle button that turns the screen to the dark mode when clicked & light mode when clicked again
 let modeBtn = document.querySelector('#mode');
+
+let currMode = "light";
 modeBtn.addEventListener("click",()=>{
     console.log("You are trying to change the mode")
 })
