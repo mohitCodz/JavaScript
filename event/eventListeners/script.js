861583b34938f75eb2b22199a9d28489902ba6b8
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-06 03:23:14 +0530

    Update script.js

--- event/eventListeners/script.js
+++ event/eventListeners/script.js
@@ -1,4 +1,4 @@
-// we can add multiple 
+// we can add multiple event listeners
 let btn1 = document.querySelector('#btn1'); 
 btn1.addEventListener("click",() => {
     console.log("button 1 was clicked ");
