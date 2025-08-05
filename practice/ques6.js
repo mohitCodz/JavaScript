b7e7e767abacddbf7cacf343c01d31f5ddab00c1
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-06 03:33:12 +0530

    Update ques6.js

--- practice/ques6.js
+++ practice/ques6.js
@@ -1,1 +1,1 @@
-// create a toggle button that turns the screen to the dark mode when clicked
+// create a toggle button that turns the screen to the dark mode when clicked & light mode when clicked again
