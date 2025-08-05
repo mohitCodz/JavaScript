a5e0ac513121668c5ea87b500a1d7dccd98270f5
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-06 03:50:19 +0530

    Update ques6.js

--- practice/ques6.js
+++ practice/ques6.js
@@ -1,16 +1,16 @@
 // create a toggle button that turns the screen to the dark mode when clicked & light mode when clicked again
 let modeBtn = document.querySelector('#mode');
+let body = document.querySelector("body")
 
 let currMode = "light";
 modeBtn.addEventListener("click",()=>{
     if( currMode == "light"){
         currMode = "dark";
-        document.querySelector("body").style.backgroundColor = "black";
+       body.style.backgroundColor = "black";
     }
     else{
  currMode = "light";
-
-        document.querySelector("body").style.backgroundColor = "white";
+body.style.backgroundColor = "white";
     }
     console.log(currMode);
 })
