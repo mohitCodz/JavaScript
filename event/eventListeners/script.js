47866e1f2a4ccf5f820842ac4710d12f475f83bc
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-06 03:27:12 +0530

    Update script.js

--- event/eventListeners/script.js
+++ event/eventListeners/script.js
@@ -12,7 +12,6 @@ btn1.addEventListener("click",() => {
     console.log("button 1 was clicked -3");
 })
 
-let btn1 = document.querySelector('#btn1'); 
 btn1.addEventListener("click",() => {
     console.log("button 1 was clicked -4");
 })
