1af34d9e883d102121ae4ba64434605e24125cd2
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-06 03:42:35 +0530

    Update ques6.js

--- practice/ques6.js
+++ practice/ques6.js
@@ -5,8 +5,11 @@ let currMode = "light";
 modeBtn.addEventListener("click",()=>{
     if( currMode == "light"){
         currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
     }
     else{
- currMode = "light"
+ currMode = "light";
+
+        document.querySelector("body").style.backgroundColor = "white";
     }
 })
