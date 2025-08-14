98abcb777c7b4d4f0285a67821c33a89d9780f67
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 16:35:46 +0530

    Update calculateTax.js

--- clases&Objects/calculateTax.js
+++ clases&Objects/calculateTax.js
@@ -1,31 +1,31 @@
 // use of prototype
 const employee = {
-    calTax(){
+    calTax() {
         console.log("The tax rate is 12%");
     },
 };
 // new employee1
 const newEmployee1 = {
-     salary: 50000,
+    salary: 50000,
 };
-
-// now we are using the function and meathods of the object1 ( employee ) in the object 2 (newEmployee)
-
-newEmployee1.__proto__ = employee;
-
 // new employee2
 const newEmployee2 = {
-     salary: 50000,
+    salary: 50000,
 };
-newEmployee2.__proto__ = employee;
+
 
 // new employee3
 const newEmployee3 = {
-     salary: 50000,
+    salary: 50000,
 };
-newEmployee3.__proto__ = employee;
+
 // newemployee4
 const newEmployee4 = {
-     salary: 50000,
+    salary: 50000,
 };
-newEmployee4.__proto__ =employee;
+
+// now we are creating prototypes
+newEmployee1.__proto__ = employee;
+newEmployee2.__proto__ = employee;
+newEmployee3.__proto__ = employee;
+newEmployee4.__proto__ = employee;
