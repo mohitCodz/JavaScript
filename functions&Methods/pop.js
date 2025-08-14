a1af15dac678db868c5328de059c4778bd485318
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 15:48:19 +0530

    Create pop.js

---
+++ functions&Methods/pop.js
@@ -0,0 +1,7 @@
+let carNames = ["Toyota", "Honda", "Ford", "Chevrolet", "Nissan"];
+console.log("Original car names:"); 
+
+let deletedCar = carNames.pop();
+carNames.pop();
+console.log("Updated car names:");
+console.log("deleted car" , deletedCar);
