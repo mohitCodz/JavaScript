7dc6da0a31ba94d5a45f77c2b9c7551f6f821c3d
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 15:48:17 +0530

    Delete pop.js

--- pop.js
+++
@@ -1,7 +0,0 @@
-let carNames = ["Toyota", "Honda", "Ford", "Chevrolet", "Nissan"];
-console.log("Original car names:"); 
-
-let deletedCar = carNames.pop();
-carNames.pop();
-console.log("Updated car names:");
-console.log("deleted car" , deletedCar);
