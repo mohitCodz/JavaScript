8351fb759be918101874a0d0e2849325fc5497a1
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 17:17:05 +0530

    Update class.js

--- clases&Objects/classes/class.js
+++ clases&Objects/classes/class.js
@@ -2,8 +2,16 @@ class toyotaCar {
     start() {
         console.log("start");
     }
-
     stop() {
         console.log("stop");
     }
+    modelYear(yerar) {
+        this.modelYear = 2025;
+    }
 };
+
+// creating object
+// now fortuner is made on the basis of toyotaCar
+let fortuner = new toyotaCar();
+let innova = new toyotaCar();
+let glanza = new toyotaCar();
