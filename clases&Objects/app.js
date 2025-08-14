13a567ac331f66a5f5c41c11ea09dd103b78eda6
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 16:04:15 +0530

    Update app.js

--- clases&Objects/app.js
+++ clases&Objects/app.js
@@ -2,6 +2,6 @@ const student = {
  fullName : "Mohit",
  marks : 90.5,
  printMarks: function() {
-    console.log("marks =",this.marks);
+    console.log("marks =",this.marks); // when we write this in any object - this.marks means student.marks
  },
 };
