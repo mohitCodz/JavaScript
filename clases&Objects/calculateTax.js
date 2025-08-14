769aa389d193a2393c810666ff9236e7b801d7f9
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 16:32:40 +0530

    Update calculateTax.js

--- clases&Objects/calculateTax.js
+++ clases&Objects/calculateTax.js
@@ -4,11 +4,25 @@ const employee = {
         console.log("The tax rate is 12%");
     },
 };
-
-const newEmployee = {
+// new employee1
+const newEmployee1 = {
      salary: 50000,
 };
 
 // now we are using the function and meathods of the object1 ( employee ) in the object 2 (newEmployee)
 
-newEmployee.__proto__ = employee;
+newEmployee1.__proto__ = employee;
+
+// new employee2
+const newEmployee2 = {
+     salary: 50000,
+};
+
+// new employee3
+const newEmployee3 = {
+     salary: 50000,
+};
+// newemployee4
+const newEmployee4 = {
+     salary: 50000,
+};
