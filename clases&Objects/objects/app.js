eb522c577552b40ec3c0438ebca3b38c2728ed16
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 17:03:01 +0530

    Create app.js

---
+++ clases&Objects/objects/app.js
@@ -0,0 +1,7 @@
+const student = {
+ fullName : "Mohit",
+ marks : 90.5,
+ printMarks: function() {
+    console.log("marks =",this.marks); // when we write this in any object - this.marks means student.marks
+ },
+};
