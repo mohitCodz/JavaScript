37eeb7ca9cbc71fc433fec55b098751ef4694ef1
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 16:29:13 +0530

    Update calculateTax.js

--- clases&Objects/calculateTax.js
+++ clases&Objects/calculateTax.js
@@ -1,5 +1,14 @@
+// use of prototype
 const employee = {
     calTax(){
         console.log("The tax rate is 12%");
     },
 };
+
+const newEmployee = {
+     salary: 50000,
+};
+
+// now we are using the function and meathods of the object1 ( employee ) in the object 2 (newEmployee)
+
+newEmployee.__proto__ = employee;
