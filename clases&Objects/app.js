22542335f1c14d62885e6ac60142fa5f91b200cc
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 17:02:54 +0530

    Delete app.js

--- clases&Objects/app.js
+++
@@ -1,7 +0,0 @@
-const student = {
- fullName : "Mohit",
- marks : 90.5,
- printMarks: function() {
-    console.log("marks =",this.marks); // when we write this in any object - this.marks means student.marks
- },
-};
