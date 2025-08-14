cada2c26b5d99ac467fa2e2dc229826aa6d690ba
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-14 16:42:01 +0530

    Update calculateTax.js

--- clases&Objects/calculateTax.js
+++ clases&Objects/calculateTax.js
@@ -7,25 +7,10 @@ const employee = {
 // new employee1
 const newEmployee1 = {
     salary: 50000,
+    // when we call calTax() it will call the 
+    calTax() {
+          console.log("The tax rate is 20%");
+    },
 };
-// new employee2
-const newEmployee2 = {
-    salary: 50000,
-};
-
-
-// new employee3
-const newEmployee3 = {
-    salary: 50000,
-};
-
-// newemployee4
-const newEmployee4 = {
-    salary: 50000,
-};
-
 // now we are creating prototypes
 newEmployee1.__proto__ = employee;
-newEmployee2.__proto__ = employee;
-newEmployee3.__proto__ = employee;
-newEmployee4.__proto__ = employee;
