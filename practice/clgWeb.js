3c88c1d2843bcadd3c0e9680d31d1845dca0cb86
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-15 23:53:52 +0530

    Update clgWeb.js

--- practice/clgWeb.js
+++ practice/clgWeb.js
@@ -1,1 +1,13 @@
-// 
+let DATA = "Secret info"; // just for example
+// class user 
+class User {
+    // constructor
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+    // meathod for viewing the data
+    viewData() {
+        console.log("Viewing the data",DATA); // seeing the data
+    }
+}
