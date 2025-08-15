8a6f07a34147db47af3ac4d220e4471363071614
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-15 19:42:23 +0530

    Update script.js

--- clases&Objects/inheritance/script.js
+++ clases&Objects/inheritance/script.js
@@ -1,31 +1,7 @@
-// class
+// parent class
 
-class parent {
-    // constructor
-    constructor(brandName,year ) {
-        console.log("Constructor is now creating the objcts");
-        this.year = year;
-        this.brandName = brandName;
-    }
-
-    //start meathod
-    start() {
-        console.log("Start");
-    }
-    // stop meathod
-    stop() {
-        console.log("Stop");
-    }
-    // model year meathod
-    modelYear() {
-        this.year = year; // storing the year 
-    }
-    // brand name
-    brandName() {
-        this.name = this.name; // storing the brand name
+class Parent{
+    hello(){
+        console.log("Hello i am the parent class");
     }
 }
-// creating object
-let fortuner = new Car("Toyota", "2026"); // constructor will invoke
-let city = new Car("Honda", "2026"); // constructor will invoke
-let bmwM4 = new Car("BMW", "2027");// constructor will invoke
