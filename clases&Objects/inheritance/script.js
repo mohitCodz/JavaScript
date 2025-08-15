f00eb79cfe4dd20321896c9f0f8762aaa49d4b24
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-15 19:51:09 +0530

    Update script.js

--- clases&Objects/inheritance/script.js
+++ clases&Objects/inheritance/script.js
@@ -1,12 +1,18 @@
 // parent class
-
 class Parent{
     hello(){
         console.log("Hello i am the parent class");
     }
+    bye(){
+        console.log("bye!")
+    }
 }
 // child class ( inherting the property form the parent class )
-class Child extends Parent{}
+class Child extends Parent{
+    relax(){
+        console.log("I'm a child and i am relaxing");
+    }
+}
 
 // child class obj
-let obj = new Child();
+let chunnu = new Child(); 
