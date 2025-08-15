6d7149b34f53215f8b5a9b946fc13025684468db
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-15 23:17:06 +0530

    Update super.js

---
+++ clases&Objects/constructor/super.js
@@ -0,0 +1,22 @@
+// class parent
+class Parent {
+    // constructor 
+    constructor() {
+        this.age = age;
+    }
+    eat() {
+        console.log("Eat the food now");
+    }
+}
+// child class1
+class Child extends Parent {
+    sleep() {
+        console.log("Good Night");
+    }
+}
+// child class2
+class Child2 extends Parent {
+    morning() {
+        console.log("Good Morning");
+    }
+}
