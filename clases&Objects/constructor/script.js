5919ad985edc26c7bc541e80d88fb5ed140d6e0b
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-15 19:39:52 +0530

    Update script.js

--- clases&Objects/constructor/script.js
+++ clases&Objects/constructor/script.js
@@ -1,27 +1,36 @@
-class toyotaCar {
+// class
 
+class Class {
     // constructor
-    constructor(year, milage) {
-        console.log("Constructor is creathing the new object");
+    constructor(brandName,year,milage ) {
+        console.log("Constructor is now creating the objcts");
         this.year = year;
+        this.brandName = brandName;
         this.milage = milage;
     }
 
-    // meathod
+    //start meathod
     start() {
-        console.log("start");
+        console.log("Start");
     }
-    // meathod
+    // stop meathod
     stop() {
-        console.log("stop");
+        console.log("Stop");
     }
-    // meathod
-    modelYear(year) {
-        this.year = 2025; // storing the year in the property name year
+    // model year meathod
+    modelYear() {
+        this.year = year; // storing the year 
     }
-};
-
+    // brand name
+    brandName() {
+        this.name = this.name; // storing the brand name
+    }
+    // milage
+    milage(){
+        this.milage = milage; // storing milage
+    }
+}
 // creating object
-let fortuner = new toyotaCar("2026", 27); // constructor will invoke
-let innova = new toyotaCar("2026", 31); // constructor will invoke
-let glanza = new toyotaCar("2027", 29);// constructor will invoke
+let fortuner = new Car("Toyota", "2026",20); // constructor will invoke
+let city = new Car("Honda", "2026",40); // constructor will invoke
+let bmwM4 = new Car("BMW", "2027",31);// constructor will invoke
