ccaa17106b20a9b6fa68ec0f81e57c6e9b608cb8
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-15 18:50:45 +0530

    Update script.js

--- clases&Objects/constructor/script.js
+++ clases&Objects/constructor/script.js
@@ -23,6 +23,6 @@ class toyotaCar {
 
 // creating object
 // now fortuner is made on the basis of toyotaCar
-let fortuner = new toyotaCar("2026"); // constructor will invoke
-let innova = new toyotaCar("2026"); // constructor will invoke
+let fortuner = new toyotaCar("2026",'20'); // constructor will invoke
+let innova = new toyotaCar("2026",'30'); // constructor will invoke
 let glanza = new toyotaCar("2027");// constructor will invoke
