14c52b270f5880bf92ada67f5f63b1a5217f76c4
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-16 13:53:12 +0530

    Create asynchronous.js

---
+++ clases&Objects/sync/asynchronous.js
@@ -0,0 +1,12 @@
+// displaying how synchronous work 
+console.log("one");
+console.log("two");
+console.log("three");
+console.log("four");
+
+// setTimeout is a function which runs some code after certain amount of time ( delay )
+setTimeout(() => {
+    console.log("five");
+},5000); // after 5 second this will print five
+console.log("six");
+console.log("seven");
