0a8ed464498ac76cdfa209db9979d141712c7600
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-16 13:53:09 +0530

    Update synchronous.js

--- clases&Objects/sync/synchronous.js
+++ clases&Objects/sync/synchronous.js
@@ -3,7 +3,3 @@ console.log("one");
 console.log("two");
 console.log("three");
 console.log("four");
-// setTimeout is a function which runs some code after certain amount of time ( delay )
-setTimeout(() => {
-    console.log("five");
-},5000); // after 5 second this will print five
