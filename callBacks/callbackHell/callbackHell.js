c346b4dcb6e711f079f7873cc9644f525fe317fc
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-16 14:27:55 +0530

    Update callbackHell.js

--- callBacks/callbackHell/callbackHell.js
+++ callBacks/callbackHell/callbackHell.js
@@ -2,5 +2,6 @@
 function getData(dataId,getNextData) {
     setTimeout(() => { // delay for 2 second
         console.log("data", dataId);
+        getNextData(); // function calling 
     },2000);
 }
