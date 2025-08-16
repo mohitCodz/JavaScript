6cc13e39d6b821789b49aadbdc537ea7839de8c0
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-16 14:06:40 +0530

    Update callback.js

--- callBacks/callback.js
+++ callBacks/callback.js
@@ -9,3 +9,19 @@ function calculator(a,b,sumCallBack){
 // call
 calculator(1,2,sum); // here sum ia passsing as an argument to another function ( calculator ) this is called callback
 // don't pass it with (). only pass it with the function name
+
+// another example using arrow function
+
+// function
+function sum (a,b){
+    console.log(a+b);
+}
+// another function
+function calculator(a,b,sumCallBack){
+    sumCallBack(a,b); 
+}
+// call
+calculator(1,2,(a,b)=>{
+     console.log(a+b);
+}); // here sum ia passsing as an argument to another function ( calculator ) this is called callback
+// don't pass it with (). only pass it with the function name
