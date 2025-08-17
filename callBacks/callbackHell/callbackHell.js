040dbef38970ff932c32aad26ae9ff4d8fa158d3
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-17 14:47:12 +0530

    Update callbackHell.js

--- callBacks/callbackHell/callbackHell.js
+++ callBacks/callbackHell/callbackHell.js
@@ -1,18 +1,19 @@
 // fucntion
-function getData(dataId,getNextData) {
+function getData(dataId, getNextData) {
     // setTimeout to delay fro 2 second
-    setTimeout(() => { 
+    setTimeout(() => {
         console.log("data", dataId);
         // condition
-        if (getNextData){
+        if (getNextData) {
             getNextData(); // if this funcitonn exists only then this function will call
         }
-    },2000);
+    }, 2000);
 }
-getData(1,()=>{
-    getData(2, ()=>{
-        getData(3, ()=>{
-            getData(4); 
+// the code below is called callback hell
+getData(1, () => {
+    getData(2, () => {
+        getData(3, () => {
+            getData(4);
         });
     })
 }); // here we have passed the same function for the nextdata 
