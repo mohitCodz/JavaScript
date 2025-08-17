f98f00f95b279a64dfd37d0b8169172a0ff6acef
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-17 15:20:36 +0530

    Update promise.js

--- callBacks/promises/promise.js
+++ callBacks/promises/promise.js
@@ -4,17 +4,16 @@
 // });
 
 
-// fucntion 
+/// fucntion
 function getData(dataId, getNextData) {
-   return new Promise ((resolve,reject)=>{
-      setTimeout(() => {
+    // setTimeout to delay fro 2 second
+    setTimeout(() => {
         console.log("data", dataId);
-        resolve("Success");
         // condition
         if (getNextData) {
             getNextData(); // if this funcitonn exists only then this function will call
         }
-   });
+    }, 2000);
 }
 
 
