de500f49c1ea1ca16e1742878e2aba0a59d3663d
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-17 14:57:36 +0530

    Update promise.js

--- callBacks/promises/promise.js
+++ callBacks/promises/promise.js
@@ -1,19 +1,41 @@
-// fucntion
-function getData(dataId, getNextData) {
-    // setTimeout to delay fro 2 second
-    setTimeout(() => {
-        console.log("data", dataId);
-        // condition
-        if (getNextData) {
-            getNextData(); // if this funcitonn exists only then this function will call
-        }
-    }, 2000);
-}
-// the code below is called callback hell ( multiple callbacks inside each other )
-getData(1, () => {
-    getData(2, () => {
-        getData(3, () => {
-            getData(4);
-        });
-    })
-}); // here we have passed the same function for the nextdata 
+// crea
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // fucntion
+// function getData(dataId, getNextData) {
+//     // setTimeout to delay fro 2 second
+//     setTimeout(() => {
+//         console.log("data", dataId);
+//         // condition
+//         if (getNextData) {
+//             getNextData(); // if this funcitonn exists only then this function will call
+//         }
+//     }, 2000);
+// }
+// // the code below is called callback hell ( multiple callbacks inside each other )
+// getData(1, () => {
+//     getData(2, () => {
+//         getData(3, () => {
+//             getData(4);
+//         });
+//     })
+// }); // here we have passed the same function for the nextdata 
