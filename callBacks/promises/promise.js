6b449dc82914758916e2b5310a3cd1ccea37edde
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-17 15:53:32 +0530

    Update promise.js

--- callBacks/promises/promise.js
+++ callBacks/promises/promise.js
@@ -1,11 +1,12 @@
 const getPromise = () => {
-    new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         console.log("this is promise");
         resolve("Success");
     });
 };
 let promise = getPromise();
 // using then meathod 
+// if the promise is fullfield then this will be exacuted
 promise.then(() => {
     console.log("Promise is fullfield");
 });
