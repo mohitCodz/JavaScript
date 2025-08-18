48753b8b622640b88326d1e65869fdb95c90c9f3
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-18 22:39:47 +0530

    Update chaining.js

--- callBacks/promises/promiseChaining/chaining.js
+++ callBacks/promises/promiseChaining/chaining.js
@@ -2,7 +2,7 @@
 function asyncFunc() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("Some data1");
+            console.log("");
             resolve("Success");
         }, 4000);
     })
