f98139f0f9c14ab67509a66c6dfd4d293295e4cd
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-18 23:46:56 +0530

    Update asyncAwait.js

--- AsyncAwait/asyncAwait.js
+++ AsyncAwait/asyncAwait.js
@@ -4,10 +4,15 @@
 // }
 
 function api() {
-    setTimeout(()=>{
+    setTimeout(() => {
         eturn new PerformanceMeasure((resolve, reject) => {
         console.log("Weather Data");
         resolve(200); // here 200 means/represents a sucessfull api call
-    },2000);
+    }, 2000);
     });
+}
+
+// async function
+async function getWeatherData() {
+    await api(); // we only use await keyword in async function
 }
