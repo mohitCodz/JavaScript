4ed43ab6d08f66f2f4b35dc852abaef370e1a5ef
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-19 00:33:42 +0530

    Update asyncAwait.js

--- AsyncAwait/asyncAwait.js
+++ AsyncAwait/asyncAwait.js
@@ -37,4 +37,16 @@ function getData(dataIDd) {
 //      console.log("Got all the data !");
 // }
 
-//
+//we can't execute the async function we need to put this function inside any function so to solve this we use IIFE
+
+// IIFE 
+// simply put the async function inside ()();
+(async function () {
+    console.log("Getting data 1.......");
+    await getData(1);
+     console.log("Getting data 2.......");
+    await getData(2);
+     console.log("Getting data 3.......");
+    await getData(3);
+     console.log("Got all the data !");
+})(); //IIFE
