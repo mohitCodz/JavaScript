2ad1579ab0f73447c93fe6642ff36123e876d554
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-18 23:41:41 +0530

    Update asyncAwait.js

--- AsyncAwait/asyncAwait.js
+++ AsyncAwait/asyncAwait.js
@@ -2,3 +2,9 @@
 // async function hello(){
 //     console.log("Helllo");
 // }
+
+function api() {
+    return new PerformanceMeasure((resolve, reject) => {
+        console.log("Weather Data");
+    });
+}
