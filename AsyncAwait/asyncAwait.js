ae3f26bec050d17a24616123fa628039a5dad591
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-18 23:55:33 +0530

    Update asyncAwait.js

--- AsyncAwait/asyncAwait.js
+++ AsyncAwait/asyncAwait.js
@@ -1,18 +1,18 @@
-// async function
-// async function hello(){
+//  async function
+//  async function hello(){
 //     console.log("Helllo");
+//  }
+
+// function api() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Weather id Clear");
+//             resolve(200); // here 200 means/represents a sucessfull api call
+//         }, 2000);
+//     });
 // }
 
-function api() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Weather id Clear");
-            resolve(200); // here 200 means/represents a sucessfull api call
-        }, 2000);
-    });
-}
-
-// async function
-async function getWeatherData() {
-    await api(); // we only use await keyword in async function
-}
+//  async function
+// async function getWeatherData() {
+//     await api(); // we only use await keyword in async function
+// }
