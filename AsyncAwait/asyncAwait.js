186b9951e067a308fa61974b4da64e5380f7a7e5
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-19 00:04:28 +0530

    Update asyncAwait.js

--- AsyncAwait/asyncAwait.js
+++ AsyncAwait/asyncAwait.js
@@ -28,5 +28,11 @@ function getData(dataIDd) {
 
 // async await
 async function getAllData() {
-    
+    console.log("Getting data 1.......");
+    await getData(1);
+     console.log("Getting data 2.......");
+    await getData(2);
+     console.log("Getting data 3.......");
+    await getData(3);
+     console.log("Got all the data !");
 }
