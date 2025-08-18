1be0e5b62dbf80a8f63a5f3e82ac53d57c21f9ff
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-18 22:48:44 +0530

    Update chaining.js

--- callBacks/promises/promiseChaining/chaining.js
+++ callBacks/promises/promiseChaining/chaining.js
@@ -7,6 +7,16 @@ function asyncFunc() {
         }, 4000);
     })
 }
+// function - suppose this as an api -we only deal with promises
+function asyncFunc() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data 2");
+            resolve("Success");
+        }, 4000);
+    })
+}
+
 // dealing with promises
 console.log("Getting data 1............");
 let p1 = asyncFunc();
