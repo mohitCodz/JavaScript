ec4b03f08bd07d4308807b8698f0878ca5958f3c
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-19 13:50:11 +0530

    Update fetchApi.js

--- fetchAPI/fetchApi.js
+++ fetchAPI/fetchApi.js
@@ -6,4 +6,5 @@ const getFact = async () => {
     console.log("Fetching data please wait....");
     let response = await fetch(URL);
     console.log(response); // the response will be in JSON format
+    // using json ()
 }
