edebadfa6d86eb0449f519207e1bdd08e2eebc2c
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-19 13:53:08 +0530

    Update fetchApi.js

--- fetchAPI/fetchApi.js
+++ fetchAPI/fetchApi.js
@@ -4,7 +4,7 @@ const URL = "https://alexwohlbruck.github.io/cat-facts/"
 const getFact = async () => {
     // featch meathod
     console.log("Fetching data please wait....");
-    let response = await fetch(URL); // 1st promise
+    let response = await fetch(URL); // 1st promise return
     console.log(response); // the response will be in JSON format
     // using json () meathod
     let data = await response.json(); // 2nd promise return
