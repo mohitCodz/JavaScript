8b60ddc16a322d7d503cd784a56157f3aa9ee8da
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-19 14:05:23 +0530

    Update fetchApi.js

--- fetchAPI/fetchApi.js
+++ fetchAPI/fetchApi.js
@@ -5,7 +5,7 @@ const getFact = async () => {
     // featch meathod
     console.log("Fetching data please wait....");
     let response = await fetch(URL); // 1st promise return
-    console.log(response); // the response will be in JSON format
+    console.log(response); // JSON format
     // using json () meathod
     let data = await response.json(); // 2nd promise return
     console.log(data[0]); // data is an object with a 'fact' property
