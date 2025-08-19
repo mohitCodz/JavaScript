a83692f986796de6222f70de30bd119e09d26c38
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-19 13:37:48 +0530

    Update fetchApi.js

--- fetchAPI/fetchApi.js
+++ fetchAPI/fetchApi.js
@@ -3,7 +3,7 @@ const URL = "https://alexwohlbruck.github.io/cat-facts/"
 
 const getFact = async () => {
 // featch meathod
-let promise = fetch(URL);
+let res = await fetch(URL);
 console.log(promise);
 
 }
